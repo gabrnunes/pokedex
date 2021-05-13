a407875e547a38ce0bec5e8ae3b1f3312bc20e37
Gabriel Nunes <gnunesmail@gmail.com>
2021-05-13 14:49:10 -0300

    style: ajuste responsividade

--- components/presentation/search-form/index.js
+++ components/presentation/search-form/index.js
@@ -8,11 +8,10 @@ export default function SearchForm({
     return (
         <>
         <form className={styles.form} onSubmit={onSubmit}>
-            <div className={styles.loading}>
-                {loading && <img src="/loading-pika.gif" width="45" height="32" />}
-            </div>
             <input className={styles.input} type="text" placeholder="Enter a Pokémon name or number" onChange={onChange} />
-            <button className={styles.button} type="submit">Search</button>
+            <button className={styles.button} type="submit">
+                {loading ? <img src="/loading-pika.gif" width="24" height="16" /> : <>Search</>}
+            </button>
         </form>
         <Link href='/all/1'>
             <a className="default-link">
